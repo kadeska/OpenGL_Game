cfb2876a3bc6a68c9594ee88e25637e6c43f06e5
Kadeska <kadeska23@gmail.com>
2025-12-27 21:35:43 -0700

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,6 @@ using::ProgramLogger::log;
 using::ProgramLogger::LogLevel;
 
 
-//#include <assimp/Importer.hpp>
-//Assimp::Importer importer;
-
-
-
-//Game3D* myGame3D;
-
 // Must be called after loading OpenGL functions.
 // In this case, after the window loads OpenGL, 
 // which takes place in the constructor of Game3D.
@@ -26,36 +19,27 @@ void printMaxVertexAttributes()
 	log("Maximum number of vertex attributes supported: " + std::to_string(nrAttributes));
 }
 
+
 int main(int argc, char* argv[])
 {
 	log("Main entry", LogLevel::DEBUG);
 
+	// I should first initialize a system file manager for cross-platform file handling.
+	// render loading screen? ---------------
+	// load assets --------------------------
+	// enter main menu state ----------------
+	// game loop ----------------------------
+	// --------------------------------------
+
+
+	// initialize game
 	Game3D* myGame3D = new Game3D();
+	// print max vertex attributes for debugging
 	printMaxVertexAttributes();
+	// start game loop
 	myGame3D->start();
+	// clean up and exit
 	myGame3D->stop();
 	
     return 0;
 }
-
-
-
-//int loadFreeType() 
-//{
-//	log("Loading FreeType...");
-//	FT_Library ft;
-//	if (FT_Init_FreeType(&ft))
-//	{
-//		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-//		return -1;
-//	}
-//
-//	//FT_Face face;
-//	//if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
-//	//{
-//	//	std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-//	//	return -1;
-//	//}
-//
-//	//FT_Set_Pixel_Sizes(face, 0, 48);
-//}
